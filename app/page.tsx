bbe5891ea58af9de1f66d9a0310b1e0e5e0fcc9c
psm1st <psm1st@sookmyung.ac.kr>
2026-01-01 23:03:41 +0900

    feat: header 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import SearchBar from './components/SearchBar';
+import Header from './components/Header';
 
 export default function Home() {
   return (
-    <div className="w-full max-w-[1440px] mx-auto min-h-screen px-4 py-8">
-      <SearchBar />
+    <div className="w-full min-h-screen">
+      <Header />
+      <div className="w-full max-w-[1440px] mx-auto px-4 py-8">
+        <SearchBar />
+      </div>
     </div>
   );
 }
